6d59c40dde08e7d9e43d7f822f95b6fa8ac90f16
RajeshThangapandi <126014783+RajeshThangapandi@users.noreply.github.com>
2024-08-13 22:25:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const app = express();
 const cors = require("cors");
 const connection = require("./db");
 
+const DataRoutes = require("./routes/data");
 
-const DataRoutes=require("./routes/data")
-// database connection
+// Database connection
 connection();
 
-// middlewares
+// CORS options
+const corsOptions = {
+  origin: 'https://buyogoclient.vercel.app', // Replace with your client’s origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions)); // Apply CORS options
 
-// routes
-
-app.use("/api/data",DataRoutes)
+// Routes
+app.use("/api/data", DataRoutes);
 
 const port = process.env.PORT || 8080;
-app.listen(port, console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
